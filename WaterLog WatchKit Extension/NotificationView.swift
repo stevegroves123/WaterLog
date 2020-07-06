7582b87454446763b6c7733a0210a3d11ae38bc5
Steven Groves <stevegroves123@gmail.com>
2020-07-06 20:34:49 +0100

    Initial Commit

---
+++ WaterLog WatchKit Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WaterLog WatchKit Extension
+//
+//  Created by steve groves on 06/07/2020.
+//  Copyright © 2020 steve groves. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
