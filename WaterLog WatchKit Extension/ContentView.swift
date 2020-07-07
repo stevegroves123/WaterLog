8fb411a24a3d549dc6cd8d9fb058091b129e6281
Steven Groves <stevegroves123@gmail.com>
2020-07-07 20:47:31 +0100

    Initial commit

--- WaterLog WatchKit Extension/ContentView.swift
+++ WaterLog WatchKit Extension/ContentView.swift
@@ -9,8 +9,32 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State var waterValue = 1.0
+    @State var waterDrankToday = 0
+    
     var body: some View {
-        Text("Hello, World!")
+        VStack{
+            Text("Water to add")
+            chooseWaterValue(waterValue: self.$waterValue)
+            Button(action:
+                {
+                    self.waterDrankToday = self.waterDrankToday + Int(self.waterValue)
+                }){
+                    HStack {
+                        Image(systemName: "plus.circle")
+                        Text("water")
+                    }
+            }
+                .background(Color.gray)
+                .cornerRadius(15)
+                .padding()
+            Spacer()
+            Text("Total Water")
+            Text("\(self.waterDrankToday*100)")
+                .foregroundColor(.blue)
+                .font(.title)
+            Spacer()
+        }
     }
 }
 
---
+++ WaterLog WatchKit Extension/chooseWaterValue.swift
@@ -0,0 +1,27 @@
+//
+//  chooseWaterValue.swift
+//  WaterLog WatchKit Extension
+//
+//  Created by steve groves on 07/07/2020.
+//  Copyright © 2020 steve groves. All rights reserved.
+//
+
+import SwiftUI
+
+struct chooseWaterValue: View {
+    @Binding var waterValue: Double
+    
+    var body: some View {
+        VStack {
+            Text("\(Int(waterValue)*100)")
+            }
+            .focusable(true)
+            .digitalCrownRotation($waterValue, from: 1, through: 10, by: 1, sensitivity: .medium, isContinuous: false, isHapticFeedbackEnabled: true)
+    }
+}
+
+struct chooseWaterValue_Previews: PreviewProvider {
+    static var previews: some View {
+        chooseWaterValue(waterValue: .constant(1.0))
+    }
+}
