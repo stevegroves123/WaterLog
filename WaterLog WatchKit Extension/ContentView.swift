21a630305aaa45d9a4b2f35d83ccfb64463381c5
Steven Groves <stevegroves123@gmail.com>
2020-07-23 21:16:46 +0100

    Update to READ the last 24hours of water data and show the sum on the watch screen. - Many Many Bugs

--- WaterLog WatchKit Extension/ContentView.swift
+++ WaterLog WatchKit Extension/ContentView.swift
@@ -12,19 +12,25 @@ import HealthKit
 struct ContentView: View {
     @State var waterValue = 1.0
     @State var waterDrankToday = 0
+    @State var waterTotal = 0
     
     var body: some View {
         VStack{
+//            readWater()
             Text("Water to add")
             chooseWaterValue(waterValue: self.$waterValue)
             Button(action:
                 {
-                    self.waterDrankToday = self.waterDrankToday + Int(self.waterValue);
                     self.writeWater()
+                    self.readWater()
+                    
                 }){
                     HStack {
                         Image(systemName: "plus.circle")
+                            .foregroundColor(Color.black)
+                            .font(.headline)
                         Text("water")
+                            .foregroundColor(Color.black)
                     }
             }
                 .background(Color.gray)
@@ -32,18 +38,14 @@ struct ContentView: View {
                 .padding()
             Spacer()
             Text("Total Water")
-            Text("\(self.waterDrankToday*100)")
+            Text("\(self.waterTotal)")
                 .foregroundColor(.blue)
                 .font(.title)
-// added to reset the values - could be used for new day/new week
-                .onTapGesture(count: 2, perform: {
-                    self.waterDrankToday = 0
-                    self.waterValue = 1.0
-                })
             Spacer()
         }.onAppear(perform: { self.checkHK() })
     }
     
+    // check for access to HealthKit then read any existing water value
     func checkHK() {
         if HKHealthStore.isHealthDataAvailable() {
             // Add code to use HealthKit here.
@@ -52,9 +54,35 @@ struct ContentView: View {
             healthStore.requestAuthorization(toShare: allTypes, read: allTypes) { (success, error) in
                 if !success { print("It went wrong") }
             }
+            self.readWater()
         }
     }
     
+    // query to read all water vales for the past 24 hrs and make waterTotal the sum
+    func readWater() {
+        guard let waterType = HKSampleType.quantityType(forIdentifier: .dietaryWater) else {
+            print("Unable to read the water")
+            return
+        }
+        
+        let todaysWater = HKQuery.predicateForSamples(withStart: (Date()-86400), end: Date(), options: .strictEndDate)
+        let waterQuery = HKSampleQuery(sampleType: waterType,predicate: todaysWater, limit: HKObjectQueryNoLimit, sortDescriptors: nil) {
+                (query, sample, error) in
+            
+                guard error == nil,
+                    let quantityWaterSamples = sample as? [HKQuantitySample] else {
+                      print("Something went wrong: \(String(describing: error))")
+                        return
+            }
+            self.waterTotal = Int(quantityWaterSamples.reduce(0.0) { $0 + $1.quantity.doubleValue(for: HKUnit.literUnit(with: .milli)) })
+            print("read water - ok")
+            DispatchQueue.main.async {
+            }
+        }
+        HKHealthStore().execute(waterQuery)
+    }
+    
+    // function to write the chosen waterValue
     func writeWater() {
         guard let waterType = HKSampleType.quantityType(forIdentifier: .dietaryWater) else {
             print("Sample type not available")
@@ -62,9 +90,8 @@ struct ContentView: View {
         }
         
         let waterQuantity = HKQuantity(unit: HKUnit.literUnit(with: .milli), doubleValue: Double(Int(waterValue)*100))
-        let today = Date()
-        let waterQuantitySample = HKQuantitySample(type: waterType, quantity: waterQuantity, start: today, end: today)
-        
+        let waterQuantitySample = HKQuantitySample(type: waterType, quantity: waterQuantity, start: Date(), end: Date())
+
         HKHealthStore().save(waterQuantitySample) { (success, error) in
             print("HK write finished - success: \(success); error: \(String(describing: error))")
         }
